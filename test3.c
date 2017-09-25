89cbd9d43ef67cdfaff88bdb9c7bdef8f95484a7
平田 智剛 <17ec084@ms.dendai.ac.jp>
2017-09-25 14:36:11 +0900

    Add files via upload

---
+++ test3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main(void)
+{
+int i;
+scanf("%d",&i);
+printf("%d==5‚Í%d\n",i,i==5);
+printf("!%d‚Í%d\n",i,!i);
+printf("%d!=5‚Í%d\n",i,i!=5);
+printf("%d<=5‚Í%d\n",i,i<=5); 
+printf("%d>=5‚Í%d\n",i,i>=5);
+printf("%d<5‚Í%d\n",i,i<5);
+printf("%d>5‚Í%d\n",i,i>5);
+printf("%d<=5&&%d>=5‚Í%d\n",i,i,i<=5&&i>=5);    
+printf("%d<=5||%d>=5‚Í%d\n",i,i,i<=5||i>=5);
+printf("!%d||%d‚Í%d,i,i-4",!i||i-4); 
+
+
+}
